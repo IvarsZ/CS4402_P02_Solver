0fbabc051dbba40265a1e3b4786f8dc33235095c
Ivars Zubkans <iz1@st-andrews.ac.uk>
2014-04-15 19:28:30 +0100

    Maven setup

---
+++ src/main/java/solver/Solver.java
@@ -0,0 +1,9 @@
+package solver;
+
+public class Solver 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/solver/SolverTest.java
@@ -0,0 +1,38 @@
+package solver;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * TODO - Unit test for solver.
+ */
+public class SolverTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public SolverTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( SolverTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
